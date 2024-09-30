b30967ddf04f3897bc5b84a81898cc4eb76a80e1
Hélio  Levi <heliolevi@gmail.com>
2024-09-30 11:39:12 -0300

    Atividade funcionarios

---
+++ atividades/funcionario/principal.py
@@ -0,0 +1,11 @@
+from funcionario import funcionario
+
+funcionario1 = funcionario("Hélio lindo", "Desenvolvedor", 15000)
+funcionario2 = funcionario("Samuel", "Desenvolvedor", 15000)
+funcionario3 = funcionario("Denilson", "Desenvolvedor", 15000)
+
+funcionario1.exebirDados()
+funcionario1.verificarSalario()
+
+funcionario2.exebirDados()
+funcionario2.verificarSalario()
---
+++ atividades/funcionario/questão1.py
@@ -0,0 +1,17 @@
+class funcionarios: 
+
+     def __init__(self, nome, cargo, salario):
+        self.nome = nome 
+        self.cargo = cargo
+        self.salario = salario
+        
+    #Criando métodos 
+     def exebirDados(self):
+            print(f"Olá {self.nome} seu cargo é {self.cargo}, parabens voce ganha uma peixinxa de {self.salario}")
+
+     def verificarSalario(self):
+                if self.salario <= 2000:
+                    print("Direito a bônus")
+                else:
+                    print("Sem direito a bônus")
+
